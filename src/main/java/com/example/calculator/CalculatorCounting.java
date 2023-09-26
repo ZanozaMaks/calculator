a194b1a9c385fa08c57bcbd09c02bf63315397b6
Mak7im <mak7im2003@mail.ru>
2023-09-27 00:11:57 +0300

    Выводится ошибка 500 при делении на 0

--- src/main/java/com/example/calculator/CalculatorCounting.java
+++ src/main/java/com/example/calculator/CalculatorCounting.java
@@ -26,11 +26,20 @@ public class CalculatorCounting {
 
     public String divide(int num1, int num2) {
         int answer = num1 / num2;
-        if (num2 == 0)
-            return "Делить на 0 нельзя";
-        return num1 + " / " + num2 + " = " + answer;
+        if (num2 == 0) {
+            return ("Делить на 0 нельзя");
+        }
+        else
+            return num1 + " / " + num2 + " = " + answer;
     }
-
 }
 
+
+
+//        if (num2 == 0) {
+//            return "Делить на 0 нельзя";
+//     }
+//        return num1 + " / " + num2 + " = " + answer;
+//}
+//}
 
